fe7305b0089040e4c40213c91af649963b4da2c4
Roshaan Siddiqui <rofaze21@gmail.com>
2019-07-24 13:36:46 -0500

    Added See More Button, Added Search Keyword query on every page, Added real time dates and days on time filter, Added label on card to indicate when the event was created, Switched SVG picture to PNG on login screen and minimized the white screen bug.

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -9,6 +9,10 @@ import * as Firebase from 'firebase/app';
 import { Database } from '@firebase/database';
 import 'firebase/database';
 import { iconDict } from '../../Models/CategoryIconsDictionary';
+import * as moment from 'moment';
+import { create } from 'domain';
+import { filter } from 'rxjs/operators';
+
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
@@ -16,12 +20,15 @@ import { iconDict } from '../../Models/CategoryIconsDictionary';
 })
 export class CardComponent implements OnInit {
   @Input() id?;
+
+  sinceDate?:any;
   iconsMatch = iconDict
   event: Event;
   colorScheme = { "Sports": ["#FF0000", "#195DF7"], "Party": "#36454f" }
-  startTime?;
+  startTime?: Date;
   endTime?;
-
+  date?;
+  time?;
 
 
   constructor(private tab1: Tab1Page, private userService: UserService, private route: ActivatedRoute, private router: Router, private firebaseDatabase: FirebaseDatabaseService) {
@@ -29,7 +36,33 @@ export class CardComponent implements OnInit {
     // console.log(this.colorScheme["Sports"][0], "priting sports")
 
   }
+  filterTime(){
+    var hour = this.startTime.getHours()
+    if (hour >= 6 && hour < 12 ) {
+      return "Morning"
+    } else if( hour >= 12 && hour< 17) {
+      return "Afternoon"
+    } else if (hour >= 17 && hour< 20) {
+      return "Evening"
+    } else if (hour >= 20 && hour<= 24) {
+      return "Night"
+    }
+  }
 
+  fetchEvent() {
+    if (this.id) {
+      this.firebaseDatabase.getEvent(this.id).subscribe(res => {
+        this.event = res
+        this.startTime = new Date(this.event.event_startTime)
+        this.endTime = new Date(this.event.event_endTime)
+        this.date = res.event_creation_timeStamp
+        var creationDateObjectInMilliseconds = this.date.seconds * 1000 + this.date.nanoseconds / 1000000
+        var createDate = moment.unix(creationDateObjectInMilliseconds/1000).format('dddd, MMMM Do, YYYY h:mm:ss A')      
+        this.sinceDate = moment(createDate, 'dddd, MMMM Do, YYYY h:mm:ss A').fromNow(); // 8 years ago
+        this.time = this.filterTime() 
+      })
+    }
+  }
 
   ngOnInit() {
     this.fetchEvent()
@@ -41,7 +74,7 @@ export class CardComponent implements OnInit {
   }
 
   categoryClicked(event_category) {
-this.tab1.categoryPressed(event_category);
+// this.tab1.categoryPressed(event_category);
 this.router.navigateByUrl("/tabs/tab1")
   }
   showGoingList() {
@@ -49,15 +82,7 @@ this.router.navigateByUrl("/tabs/tab1")
     this.router.navigateByUrl("show-going/" + this.id)
 
   }
-  fetchEvent() {
-    if (this.id) {
-      this.firebaseDatabase.getEvent(this.id).subscribe(res => {
-        this.event = res
-        this.startTime = new Date(this.event.event_startTime)
-        this.endTime = new Date(this.event.event_endTime)
-      })
-    }
-  }
+
 
   // categoryClicked(category) {
   //   this.activity.categoryPressed(category)
